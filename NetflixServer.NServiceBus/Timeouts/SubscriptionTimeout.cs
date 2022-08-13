147451d69410c6594268815e4fe0e2cb4a2ae249
Oleksandr Shportko <alex.shportko@gmail.com>
2022-08-13 22:43:43 +0300

    Added Timeout class

---
+++ NetflixServer.NServiceBus/Timeouts/SubscriptionTimeout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetflixServer.NServiceBus.Timeouts
+{
+    public class SubscriptionTimeout
+    {
+        public string UserEmail { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public bool IsLastNotification { get; set; }
+    }
+}
