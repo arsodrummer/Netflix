223b95d1ac3dfdda7f54a1b428709a3736d26996
Oleksandr Shportko <alex.shportko@gmail.com>
2022-09-05 22:18:00 +0300

    Added CreateUserEmailContentCommand and response

---
+++ NetflixServer.Shared/Commands/CreateUserEmailContentCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace NetflixServer.Shared.Commands
+{
+    public class CreateUserEmailContentCommand
+    {
+        public NotificationType NotificationType { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
