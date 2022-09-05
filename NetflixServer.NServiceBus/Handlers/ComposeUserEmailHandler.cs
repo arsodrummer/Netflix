b0b38e0e04ced1cd3d845b44233537085a1abf07
Oleksandr Shportko <alex.shportko@gmail.com>
2022-09-05 22:18:18 +0300

    Added ComposeUserEmailHandler for CreateUserEmailContentCommand

---
+++ NetflixServer.NServiceBus/Handlers/ComposeUserEmailHandler.cs
@@ -0,0 +1,32 @@
+﻿using NetflixServer.NServiceBus.Services;
+using NetflixServer.Shared;
+using NetflixServer.Shared.Commands;
+using NServiceBus;
+using System.Threading.Tasks;
+
+namespace NetflixServer.NServiceBus.Handlers
+{
+    public class ComposeUserEmailHandler : IHandleMessages<CreateUserEmailContentCommand>
+    {
+        private readonly NotificationContentService _notificationContentService;
+
+        public ComposeUserEmailHandler(NotificationContentService notificationContentService)
+        {
+            _notificationContentService = notificationContentService;
+        }
+
+        public async Task Handle(CreateUserEmailContentCommand message, IMessageHandlerContext context)
+        {
+            (string subject, string body) = _notificationContentService.CreateUserEmailContent(message.NotificationType, message.UserName);
+
+            var sendEmailResponse = new CreateUserEmailContentResponse
+            {
+                Subject = subject,
+                Body = body,
+                Email = message.Email,
+            };
+
+            await context.Reply(sendEmailResponse);
+        }
+    }
+}
